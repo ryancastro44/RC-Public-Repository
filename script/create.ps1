39036ec2ab13fefa77f1959d8cbe5e70bf490023
Fernandez <bernie.lu@outlook.com>
2023-01-26 16:31:11 +0800

    trycatch

--- script/create.ps1
+++ script/create.ps1
@@ -1,38 +1,50 @@
-#--init
-$global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
-$json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
-$CreateCSV = Import-Csv -Path "$RootPath\create.csv"
-$counter = 0
-Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
-Write-Output "`n`n------------------BEGIN-------------------------"
-Write-Output "$(Get-Date -Format "HH:mm")[Log]: Starting init"
+try{
+    #--init
+    $global:ErrorActionPreference = "Stop"
+    $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
+    Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
+    $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
+    $CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+    $counter = 0
+    
+    Write-Output "`n`n------------------BEGIN-------------------------"
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Starting init"
 
-foreach($c in $CreateCSV){
-    #--transform
-    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Transforming data"
-    $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
+    foreach($c in $CreateCSV){
+        #--transform
+        Write-Output "$(Get-Date -Format "HH:mm")[Log]: Transforming data"
+        $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
 
-    #--assembly
-    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
-    $AssembledObj = [PSCustomObject]@{
-        Name = $NoCharsItem
-        DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
-        PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
-        Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
-        Members = ($c.Members) -split (',') #turm members to array
+        #--assembly
+        Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
+        $AssembledObj = [PSCustomObject]@{
+            Name = $NoCharsItem
+            DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
+            PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
+            Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
+            Members = ($c.Members) -split (',') #turm members to array
+        }
+
+        #--output
+        $counter++
+        Write-Output "`n------------------OUTPUT($counter)-------------------------"
+        foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
+            Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
+            $AssembledObj.$($currentItemName)
+        }
     }
 
-    #--output
-    $counter++
-    Write-Output "`n------------------OUTPUT($counter)-------------------------"
-    foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
-        Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
-        $AssembledObj.$($currentItemName)
-    }
+    Write-Output "`n`n------------------END-------------------------"
+    Stop-Transcript | Out-Null
+
 }
-
-Write-Output "`n`n------------------END-------------------------"
-Stop-Transcript | Out-Null
-
+catch{
+    $e = $_.Exception.GetType().FullName
+    $line = $_.InvocationInfo.ScriptLineNumber
+    $msg = $_.Exception.Message
+    Write-Output "$(Get-Date -Format "HH:mm")[Error]: Initialization failed at line [$line] due [$e] `n`nwith details `n`n[$msg]`n"
+    Stop-Transcript | Out-Null
+    exit
+}
 
 
