b8e0e40e76400b96ec6d526f7acecf8024841fe8
Fernandez <bernie.lu@outlook.com>
2023-01-26 16:08:56 +0800

    foreach

--- script/create.ps1
+++ script/create.ps1
@@ -1,48 +1,38 @@
 #--init
 $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
-$CreateCSV = Import-Csv -Path "$RootPath\create.csv"
-Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
-
 $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
-
+$CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+$counter = 0
+Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
 Write-Output "`n`n------------------BEGIN-------------------------"
 Write-Output "$(Get-Date -Format "HH:mm")[Log]: Starting init"
 
-#--transform
-Write-Output "$(Get-Date -Format "HH:mm")[Log]: Transforming data"
-$PurposeItem = $CreateCSV | Select-Object -ExpandProperty Purpose
-$ArrayMembersItem = ($CreateCSV | select -ExpandProperty Members) -split (',')
+foreach($c in $CreateCSV){
+    #--transform
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Transforming data"
+    $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
 
-$item = $CreateCSV | select -ExpandProperty Name
-$TrimedItem = ($item.TrimEnd()).TrimStart() #removed whitespaces
-$NoCharsItem = $TrimedItem  -replace '[\W]', '' #removed special char
-$LowerCasedItem = $NoCharsItem.ToLower() #convert to lowercasing
-$AppendedItem = $json.AliasPrefix + $LowerCasedItem #append prefix
+    #--assembly
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
+    $AssembledObj = [PSCustomObject]@{
+        Name = $NoCharsItem
+        DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
+        PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
+        Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
+        Members = ($c.Members) -split (',') #turm members to array
+    }
 
-#--assembly
-Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
-$AssembledObj = [PSCustomObject]@{
-    Name = $NoCharsItem
-    DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
-    PrimarySmtpAddress  = $AppendedItem + "@" +$json.DomainName
-    Description = "`n Created at: $env:COMPUTERNAME" + `
-    "`n Created by: $env:UserName" + `
-    "`n Created on: ($(Get-Date))" + `
-    "`n`n=========`n" + $PurposeItem
-
+    #--output
+    $counter++
+    Write-Output "`n------------------OUTPUT($counter)-------------------------"
+    foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
+        Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
+        $AssembledObj.$($currentItemName)
+    }
 }
 
-#--output
-Write-Output "`n------------------OUTPUT-------------------------"
-Write-Host "`n`n Name:" -foregroundcolor Cyan
-$AssembledObj.Name
-Write-Host "`n`n DisplayName:" -foregroundcolor Cyan
-$AssembledObj.DisplayName
-Write-Host "`n`n PrimarySmtpAddress:" -foregroundcolor Cyan
-$AssembledObj.PrimarySmtpAddress
-Write-Host "`n`n Description:" -foregroundcolor Cyan
-$AssembledObj.Description
+Write-Output "`n`n------------------END-------------------------"
+Stop-Transcript | Out-Null
 
-Write-Host "`n`n Members:" -foregroundcolor Cyan
-$ArrayMembersItem
+
 
