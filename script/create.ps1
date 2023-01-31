bf344dfd16884197246a3be0bb52477e042dcd11
Fernandez <bernie.lu@outlook.com>
2023-01-31 17:52:20 +0800

    WinForm

--- script/create.ps1
+++ script/create.ps1
@@ -1,103 +1,67 @@
-try {
-    #--init
-    $global:ErrorActionPreference = "Stop"
-    $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
-    $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
-    $counter = 0 
-    #---init>gui-util
-        Add-Type -AssemblyName PresentationCore,PresentationFramework
-        $global:YesNoButton = [System.Windows.MessageBoxButton]::YesNo
-        $global:OKButton = [System.Windows.MessageBoxButton]::OK
-        $global:InfoIcon = [System.Windows.MessageBoxImage]::Information
-        $global:WarningIcon = [System.Windows.MessageBoxImage]::Warning
-        $global:ErrorIcon = [System.Windows.MessageBoxImage]::Error
-        $global:QButton = [System.Windows.MessageBoxImage]::Question
-
-    function ClearCreateCSV {
-        Remove-Item -Path "$RootPath\create.csv"
-        New-Item $RootPath\create.csv -ItemType File | Out-Null
-        Set-Content $RootPath\create.csv 'Name,Purpose,Members'    
-    }
-
-    function Get-Kill {
-        param (
-            $Mode
-        )
-        if ($Mode -eq "Hard") {
-            $e = $_.Exception.GetType().FullName
-            $line = $_.InvocationInfo.ScriptLineNumber
-            $msg = $_.Exception.Message
-            Write-Output "$(Get-Date -Format "HH:mm")[Error]: Initialization failed at line [$line] due [$e] `n`nwith details `n`n[$msg]`n"
-            Write-Output "`n`n------------------END-------------------------"
-            Stop-Transcript | Out-Null
-            ClearCreateCSV
-            exit
-        }else{
-            Write-Output "`n`n------------------END-------------------------"
-            Stop-Transcript | Out-Null
-            ClearCreateCSV
-            exit
+function CreateDL {
+    
+    try {
+        #--init
+        $counter = 0 
+   
+        function CheckCreateCSV {
+            $global:CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+            if ($($CreateCSV.Name.Count) -gt 0) {
+                
+            }else{
+                Write-Host "`n------------------OUTPUT($counter)-------------------------"
+                Write-Host "$(Get-Date -Format "HH:mm")[Log]: CSV is empty (~_^)" -foregroundcolor Yellow
+    
+                #popup method 2
+                Write-Host "$(Get-Date -Format "HH:mm")[Debug]: Attempting to update [create.csv]"
+                $UpdateCSVResult = [System.Windows.MessageBox]::Show("Empty CSV. Do you want to update [create.csv] file?","$($json.ToolName) $($json.ToolVersion)",$YesNoButton,$QButton)
+    
+                If($UpdateCSVResult -eq "Yes")
+                {
+                    Invoke-Item "$RootPath\create.csv"
+                    Start-Sleep -s 15
+                    Write-Host "`n$(Get-Date -Format "HH:mm")[Debug]: Checking again [create.csv]"
+                    [System.Windows.MessageBox]::Show("Checking again [create.csv]","$($json.ToolName) $($json.ToolVersion)",$OKButton,$WarningIcon)
+                    CheckCreateCSV
+                }else{
+                    [System.Windows.MessageBox]::Show("Goodbye!.","$($json.ToolName) $($json.ToolVersion)",$OKButton,$WarningIcon)
+                }
+       
+            }
+        }
+        
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: Initialization success"
+        CheckCreateCSV
+        foreach($c in $CreateCSV){
+            #--transform
+            Write-Host "`n$(Get-Date -Format "HH:mm")[Log]: Transforming data"
+            $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
+            #--assembly
+            Write-Host "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
+            $AssembledObj = [PSCustomObject]@{
+                Name = $NoCharsItem
+                DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
+                PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
+                Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
+                Members = ($c.Members) -split (',') #turm members to array
+            }
+            #--output
+            $counter++
+            Write-Host "`n------------------OUTPUT($counter)-------------------------"
+            foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
+                Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
+                Write-Host "$($AssembledObj.$($currentItemName))"
+                
+            }
+            if ($($AssembledObj.Members).length -gt 0) {
+                
+            }else{
+                Write-Host "No members found" -foregroundcolor Yellow
+            }
         }
         
     }
-
-    function CheckCreateCSV {
-        $global:CreateCSV = Import-Csv -Path "$RootPath\create.csv"
-        if ($($CreateCSV.Name.Count) -gt 0) {
-            
-        }else{
-            Write-Output "`n------------------OUTPUT($counter)-------------------------"
-            Write-Host "CSV is empty (~_^)" -foregroundcolor Yellow
-
-            #popup method 2
-            Write-Output "`n$(Get-Date -Format "HH:mm")[Debug]: Attempting to update [create.csv]"
-            $UpdateCSVResult = [System.Windows.MessageBox]::Show("Empty CSV. Do you want to update [create.csv] file?","PopUp Method 2",$YesNoButton,$QButton)
-
-            If($UpdateCSVResult -eq "Yes")
-            {
-                Invoke-Item "$RootPath\create.csv"
-                Start-Sleep -s 15
-                CheckCreateCSV
-            }else{
-                [System.Windows.MessageBox]::Show("Attempting to gracefully exit the script.","PopUp Method 2",$OKButton,$WarningIcon)
-                Get-Kill
-            }
-        }
+    catch {
+        Get-Kill -Mode "Hard"
     }
-
-    Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
-    
-    Write-Output "`n`n------------------BEGIN-------------------------"
-    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Initialization success"
-    CheckCreateCSV
-    foreach($c in $CreateCSV){
-        #--transform
-        Write-Output "`n$(Get-Date -Format "HH:mm")[Log]: Transforming data"
-        $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
-        #--assembly
-        Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
-        $AssembledObj = [PSCustomObject]@{
-            Name = $NoCharsItem
-            DisplayName  = $json.DisplayNamePrefix + $NoCharsItem
-            PrimarySmtpAddress  = $($json.AliasPrefix + $($NoCharsItem.ToLower())) + "@" + $json.DomainName #join and convert to lowercases
-            Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
-            Members = ($c.Members) -split (',') #turm members to array
-        }
-        #--output
-        $counter++
-        Write-Output "`n------------------OUTPUT($counter)-------------------------"
-        foreach ($currentItemName in $(@("Name","DisplayName","PrimarySmtpAddress","Description","Members")) ) {
-            Write-Host "`n`n $($currentItemName):" -foregroundcolor Cyan
-            $AssembledObj.$($currentItemName)
-        }
-        if ($($AssembledObj.Members).length -gt 0) {
-            
-        }else{
-            Write-Host "No members found" -foregroundcolor Yellow
-        }
-    }
-    Get-Kill 
-}
-catch {
-    Get-Kill -Mode "Hard"
 }
---
+++ script/toolbox.ps1
@@ -0,0 +1,150 @@
+try {
+    #--init
+    $global:ErrorActionPreference = "Stop"
+    $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
+    $global:json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
+    
+    #---init>gui-util
+        Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+        Add-Type -AssemblyName PresentationCore,PresentationFramework
+        $global:YesNoButton = [System.Windows.MessageBoxButton]::YesNo
+        $global:OKButton = [System.Windows.MessageBoxButton]::OK
+        $global:InfoIcon = [System.Windows.MessageBoxImage]::Information
+        $global:WarningIcon = [System.Windows.MessageBoxImage]::Warning
+        $global:ErrorIcon = [System.Windows.MessageBoxImage]::Error
+        $global:QButton = [System.Windows.MessageBoxImage]::Question
+
+    
+    function global:Get-Kill {
+        param (
+            $Mode
+        )
+        if ($Mode -eq "Hard") {
+            $e = $_.Exception.GetType().FullName
+            $line = $_.InvocationInfo.ScriptLineNumber
+            $msg = $_.Exception.Message
+            Write-Output "$(Get-Date -Format "HH:mm")[Error]: Initialization failed at line [$line] due [$e] `n`nwith details `n`n[$msg]`n"
+            Write-Output "`n`n------------------END ROOT-------------------------"
+            Stop-Transcript | Out-Null
+            ClearCreateCSV
+            exit
+        }else{
+            Write-Output "`n`n------------------END ROOT-------------------------"
+            Stop-Transcript | Out-Null
+            ClearCreateCSV
+            exit
+        }
+        
+    }
+    
+    function global:ClearCreateCSV {
+        Remove-Item -Path "$RootPath\create.csv"
+        New-Item $RootPath\create.csv -ItemType File | Out-Null
+        Set-Content $RootPath\create.csv 'Name,Purpose,Members'    
+    }
+
+    Start-Transcript -Path "$RootPath\Toolbox_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
+    
+    Write-Output "`n`n------------------BEGIN ROOT-------------------------"
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Form init success"
+    
+    #--form
+    #---form-assembly
+    $form = New-Object Windows.Forms.Form -Property @{
+        Size = New-Object System.Drawing.Size(485,460)
+        Text = "$($json.ToolName) $($json.ToolVersion)"
+        StartPosition = 'CenterScreen'
+        BackColor = $json.ToolUIBackColor
+        FormBorderStyle = 'Fixed3D'
+    }
+    $lblTOA = New-Object System.Windows.Forms.Label -Property @{
+        Location = New-Object System.Drawing.Point(85,50)
+        Size = New-Object System.Drawing.Size(290,150)
+        ForeColor = $json.ToolUILabelColorDark
+        BackColor = $json.ToolUIBackColorDark
+        Text = "$($json.ToolTOAText)"
+    }
+
+
+    $btnStart = New-Object System.Windows.Forms.Button -Property @{
+        Location = New-Object System.Drawing.Point(160,240)
+        Size = New-Object System.Drawing.Size(125,50)
+        ForeColor = $json.ToolUILabelColor
+        BackColor = $json.ToolUIBtnColor
+        Text = 'START'      
+    }
+
+    $btnStart.Add_Click({
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: TOA confirm success"
+        $btnStart.Hide()
+        $lblTOA.Hide()
+        $form.Controls.Add($shpDivider)
+        $form.Controls.Add($lblMainMenu)
+        $form.Controls.Add($btnCreate)
+        $form.Controls.Add($btnRead)
+        $form.Controls.Add($btnUpdate)
+        $form.Controls.Add($btnDelete)
+    })
+
+    $shpDivider = New-Object System.Windows.Forms.Label -Property @{
+        Location = New-Object System.Drawing.Point(30,50)
+        Size = New-Object System.Drawing.Size(400,2)
+        Text = ""
+        BorderStyle = 'Fixed3D'
+    }
+    $lblMainMenu = New-Object System.Windows.Forms.Label -Property @{
+        Location = New-Object System.Drawing.Point(30,30)
+        Size = New-Object System.Drawing.Size(280,20)
+        Font = New-Object System.Drawing.Font("Microsoft Sans Serif",9,[System.Drawing.FontStyle]::Bold)
+        Text = "Group Management Tools"
+    }
+    $btnCreate = New-Object System.Windows.Forms.Button -Property @{
+        Location = New-Object System.Drawing.Point(30,70)
+        Size = New-Object System.Drawing.Size(125,50)
+        ForeColor = $json.ToolUILabelColor
+        BackColor = $json.ToolUIBtnColor
+        Text = 'CREATE'      
+    }
+
+    $btnCreate.Add_Click({
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: CREATE selected"
+        Import-Module "$RootPath\create.ps1" -Force
+        Write-Host "$(Get-Date -Format "HH:mm")[Log]: CREATE function imported"
+        CreateDL
+        Write-Host "`n`n$(Get-Date -Format "HH:mm")[Log]: CREATE function completed"
+        [System.Windows.MessageBox]::Show("CREATE function completed","$($json.ToolName) $($json.ToolVersion)",$OKButton,$InfoIcon)
+    })
+
+
+    $btnRead = New-Object System.Windows.Forms.Button -Property @{
+        Location = New-Object System.Drawing.Point(175,70)
+        Size = New-Object System.Drawing.Size(125,50)
+        ForeColor = $json.ToolUILabelColor
+        BackColor = $json.ToolUIBtnColor
+        Text = 'READ'      
+    }
+    $btnUpdate = New-Object System.Windows.Forms.Button -Property @{
+        Location = New-Object System.Drawing.Point(30,140)
+        Size = New-Object System.Drawing.Size(125,50)
+        ForeColor = $json.ToolUILabelColor
+        BackColor = $json.ToolUIBtnColor
+        Text = 'UPDATE'      
+    }
+    $btnDelete = New-Object System.Windows.Forms.Button -Property @{
+        Location = New-Object System.Drawing.Point(175,140)
+        Size = New-Object System.Drawing.Size(125,50)
+        ForeColor = $json.ToolUILabelColor
+        BackColor = $json.ToolUIBtnColor
+        Text = 'DELETE'      
+    }
+
+    #---form-render
+    $form.Controls.Add($lblTOA)
+    $form.Controls.Add($btnStart)
+    $form.ShowDialog() | Out-Null
+    
+    Get-Kill 
+}
+catch {
+    Get-Kill -Mode "Hard"
+}
