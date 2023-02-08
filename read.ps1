f67c6b5c4700f63bcfcc3f0cbc1fb4ba4d89b54a
ryancastro44 <123541240+ryancastro44@users.noreply.github.com>
2023-02-09 01:34:30 +0800

    Delete read.ps1

--- read.ps1
+++
@@ -1,5 +0,0 @@
-$csv = Import-csv "C:\Users\RyanVincentC\Desktop\RC PowerShell BootCamp\RC-Public-Repository\script\create.csv"
-
-Foreach($Mem in $csv){
-    
-    $Member = Write-Host "Members: $Mem.Members"}
